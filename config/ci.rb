4130f455f89cd3b352931b6d6a6a0180466dc844
Felipe Lima <lipezera1836@gmail.com>
2026-09-16 14:11:38 -0300

    chore: configura pipeline local de validação

--- config/ci.rb
+++ config/ci.rb
@@ -7,8 +7,7 @@ CI.run do
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
-  step "Tests: Rails", "bin/rails test"
-  step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
+  step "Tests: RSpec", "bundle exec rspec"
 
   # Optional: Run system tests
   # step "Tests: System", "bin/rails test:system"
