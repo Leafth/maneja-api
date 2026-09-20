86932a3dc8ee364102ca21e4eae20a0fec60bb88
Felipe Lima <lipezera1836@gmail.com>
2026-09-20 10:37:26 -0300

    feat: adiciona autenticação de requisições por JWT

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :auth do
         post "login", to: "sessions#create"
+        post "refresh", to: "tokens#create"
       end
 
       get "health", to: "health#show"
