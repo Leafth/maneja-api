6f32ddd1464162ccff52404705c6a799368ff0bb
Felipe Lima <lipezera1836@gmail.com>
2026-09-20 09:27:55 -0300

    feat: adiciona rotação de refresh token

---
+++ app/services/auth/refresh_session.rb
@@ -0,0 +1,25 @@
+module Auth
+  class RefreshSession
+    class << self
+      def call(refresh_token:)
+        digest = RefreshToken.digest(refresh_token)
+
+        Session.transaction do
+          session = Session.active.lock.find_by!(refresh_token_digest: digest)
+
+          raise AuthenticationError, I18n.t("errors.authentication.expired_token") unless session
+
+          new_refresh_token = RefreshToken.generate
+
+          session.update!(
+            refresh_token_digest: RefreshToken.digest(new_refresh_token),
+          )
+
+          access_token = AccessToken.encode(user: session.user, session: session)
+
+          { access_token:, refresh_token: new_refresh_token }
+        end
+      end
+    end
+  end
+end
