28d706a1da696898bc92028562381354d65fabe6
Felipe Lima <lipezera1836@gmail.com>
2026-09-20 11:08:15 -0300

    feat: adiciona endpoint de cadastro

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :auth do
+        post "register", to: "registrations#create"
         post "login", to: "sessions#create"
         post "refresh", to: "tokens#create"
         get "me", to: "sessions#show"
