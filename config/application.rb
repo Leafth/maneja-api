a8e068185aecb8fe99085d0e8a6b64979627318c
Felipe Lima <lipezera1836@gmail.com>
2026-09-20 11:22:17 -0300

    fix: corrige traduções dos atributos do usuário

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module ManejaApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :"pt-BR"
     config.i18n.available_locales = [ :"pt-BR", :en ]
 
     config.log_tags = [ :request_id ]
