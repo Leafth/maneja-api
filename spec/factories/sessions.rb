5a5ec1ba00e9e5e6152c3ea0b92bcfc37f149fcf
Felipe Lima <lipezera1836@gmail.com>
2026-09-21 16:04:49 -0300

    test: define factories de users e sessions

--- spec/factories/sessions.rb
+++ spec/factories/sessions.rb
@@ -1,8 +1,17 @@
 FactoryBot.define do
   factory :session do
-    user { nil }
-    refresh_token_digest { "MyString" }
-    expires_at { "2026-09-20 11:50:00" }
-    revoked_at { "2026-09-20 11:50:00" }
+    association :user
+
+    refresh_token_digest { SecureRandom.hex(64) }
+    expires_at { 30.days.from_now }
+    revoked_at { nil }
+
+    trait :expired do
+      expires_at { 1.minute.ago }
+    end
+
+    trait :revoked do
+      revoked_at { Time.current }
+    end
   end
 end
