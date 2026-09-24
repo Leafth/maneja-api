1a02f08ed4986341bcf7836498666639d68db150
Felipe Lima <lipezera1836@gmail.com>
2026-09-24 11:12:00 -0300

    refactor: utiliza código de 6 dígitos na recuperação de senha

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,24 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   validates :name, presence: true
+
+  protected
+
+  def set_reset_password_token
+    raw = nil
+    encoded = nil
+
+    loop do
+      raw = SecureRandom.random_number(1_000_000).to_s.rjust(6, "0")
+      encoded = Devise.token_generator.digest(self.class, :reset_password_token, raw)
+
+      break unless self.class.exists?(reset_password_token: encoded)
+    end
+
+    self.reset_password_token = encoded
+    self.reset_password_sent_at = Time.now.utc
+    save(validate: false)
+
+    raw
+  end
 end
